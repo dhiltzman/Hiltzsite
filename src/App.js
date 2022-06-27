add0517896a0c48f60063edf31bb1ad1ea18c5bf
David Hiltzman <david.hiltzman@okstate.edu>
2022-06-26 21:36:54 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route}
+    from 'react-router-dom';
+import Home from './pages';
+import About from './pages/about';
+import Blogs from './pages/blogs';
+import SignUp from './pages/signup';
+import Contact from './pages/contact';
 
 function App() {
   return (
+      <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path='/' exact element={<Home />} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='/blogs' element={<Blogs/>} />
+        <Route path='/sign-up' element={<SignUp/>} />
+      </Routes>
+      </Router>
+
+    //<p>Sup</p>
+    /*
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,6 +40,8 @@ function App() {
         </a>
       </header>
     </div>
+    );
+    */
   );
 }
 
