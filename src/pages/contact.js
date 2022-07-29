afc3e4c79990cd78aaaa77a3a8019ed1841e3d9e
David Hiltzman <david.hiltzman@okstate.edu>
2022-07-29 12:47:08 -0500

    created contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import SocialFollow from "../components/socialFollow";
+import ContactForm from "../components/contactForm";
 import "../styles.css";
+
 
 function contact() {
   return (
     <div>
       <h1>Contact Me!</h1>
       <p>Do not hesitate to reach out to me!</p>
+      <p>Follow the links below to view my work, or drop a message below!</p>
 
       <SocialFollow />
       
+      <ContactForm />
     </div>
   )
 }
