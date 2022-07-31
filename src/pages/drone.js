c920b0506dbb26f715633f59f9eba1122467e3d5
David Hiltzman <david.hiltzman@okstate.edu>
2022-07-31 16:17:14 -0500

    updated, adding photos to image gallery

--- src/pages/drone.js
+++ src/pages/drone.js
@@ -1,8 +1,11 @@
 import React from 'react'
-import mavic2Picture from '../components/documents/pictures/mavic_2_profile_v2.jpg'
-import mavic2Picturev2 from '../components/documents/pictures/mavic_2_profile_v3.jpg'
-import stillwaterOSUPic from '../components/documents/pictures/stillwaterOSUPic.png'
 import '../styles.css'
+
+import stillwaterOSUPic from '../components/documents/pictures/stillwaterOSUPic_4-11-21.png'
+import drumrightOKPic from '../components/documents/pictures/drumrightOK_4-3-21.JPG'
+import pitcherOKPic from '../components/documents/pictures/pitcherOK_3-16-22v2.jpg'
+import downtownDallasTXPic from '../components/documents/pictures/downtownDallasTX_6-18-21.png'
+
 
 function drone() {
   /* 
@@ -18,39 +21,48 @@ function drone() {
         <p>some intro stuff blah blah blah</p>
       </div>
 
-      <hr /> 
-
-     {/* https://www.w3schools.com/css/tryit.asp?filename=trycss_ex_images_text_top_left
-      <div class="container">
-        <img src={mavic2Picture}
-          alt="mavic2" width="1000" height="300" />
-        <div class="topleft">Top Left wording yada yada</div>
-      </div>
-
-      <hr /> */}
-
-      {/* https://www.w3schools.com/css/tryit.asp?filename=trycss_css_image_overlay_slideleft */}
-      <div className="rotatedImageGallery">
-        <p className="rotatedImageGalleryText">
-          Stillwater OK, overlooking Oklahoma State University. 4-11-21
-        </p>
-        <div className="swipeLeft">
-
-          <img 
-            src={stillwaterOSUPic} 
-            alt="Stillwater OK, overlooking Oklahoma State University. 4-11-21" 
-            className="image" 
-          />
-          <div className="overlay">
-            <div className="text">Stillwater OK, overlooking Oklahoma State University. 4-11-21</div>
-          </div>
-        </div>
-      </div>
       <hr />
        {/* Image Modal: https://www.w3schools.com/css/tryit.asp?filename=trycss_image_modal_js */}
+      <div className="imageGallery">
+        <div className="responsive">
+          <div className="gallery">
+            <a target="_blank" href="/home">
+              <img src={stillwaterOSUPic} alt="Stillwater, OK" width="600" height="400" />
+            </a>
+            <div className="desc">Stillwater, OK 4-11-21</div>
+          </div>
+        </div>
 
+        <div className="responsive">
+          <div className="gallery">
+            <a target="_blank" href="/home">
+              <img src={drumrightOKPic} alt="Drumright, OK" width="600" height="400" />
+            </a>
+            <div className="desc">Drumright, OK 4-3-21</div>
+          </div>
+        </div>
+        
+        <div className="responsive">
+          <div className="gallery">
+            <a target="_blank" href="/home">
+              <img src={pitcherOKPic} alt="Pitcher, OK" width="600" height="400" />
+            </a>
+            <div className="desc">Pitcher, OK 3-16-22</div>
+          </div>
+        </div>
+        
+        <div className="responsive">
+          <div className="gallery">
+            <a target="_blank" href="/home">
+              <img src={downtownDallasTXPic} alt="Downtown Dallas, OK" width="600" height="400" />
+            </a>
+            <div className="desc">Downtown Dallas, OK 6-18-21</div>
+          </div>
+        </div>
     </div>
 
+    <hr />
+    </div>
   )
 }
 
